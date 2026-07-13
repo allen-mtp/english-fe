596ac33816ade4c963722fa3661693502ae81533
allen <allen@darasa.io>
2026-07-13 15:04:49 +0400

    update

--- src/locales/i18n.js
+++ src/locales/i18n.js
@@ -1,25 +1,28 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import LanguageDetector from 'i18next-browser-languagedetector';
 
 import en from './en';
 import vi from './vi';
 
 const isServer = typeof window === 'undefined';
+const config = {
+  resources: { en: { translation: en }, vi: { translation: vi } },
+  lng: 'en',
+  fallbackLng: 'en',
+  interpolation: { escapeValue: false },
+};
 
-i18n
-  .use(isServer ? undefined : LanguageDetector)
-  .use(initReactI18next)
-  .init({
-    resources: { en: { translation: en }, vi: { translation: vi } },
-    lng: isServer ? 'en' : undefined,
-    fallbackLng: 'en',
-    interpolation: { escapeValue: false },
-    detection: {
-      order: ['localStorage', 'navigator'],
-      caches: ['localStorage'],
-      lookupLocalStorage: 'lang',
-    },
-  });
+if (!isServer) {
+  const LanguageDetector = require('i18next-browser-languagedetector').default;
+  i18n.use(LanguageDetector);
+  config.lng = undefined;
+  config.detection = {
+    order: ['localStorage', 'navigator'],
+    caches: ['localStorage'],
+    lookupLocalStorage: 'lang',
+  };
+}
+
+i18n.use(initReactI18next).init(config);
 
 export default i18n;
